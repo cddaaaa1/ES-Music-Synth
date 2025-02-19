d7e66261b562ecbe8cc3d0d99050aaff5c580adf
cddaaaa1 <yuduan@gmail.com>
2025-02-19 18:10:12 +0000

    lab1 finished

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include <STM32FreeRTOS.h> // FreeRTOS for threading support
 
 // Constants
-const uint32_t interval = 100;                           // Display update interval
-const uint32_t fs = 22000;                               // Sampling frequency (Hz)
-const uint64_t phase_accumulator_modulus = (1ULL << 32); // 2^32 using bit shift
+const uint32_t interval = 100;                         // Display update interval
+const uint32_t fs = 22000;                             // Sampling frequency (Hz)
+const uint64_t phase_accumulator_modulus = 4294967296; // 2^32 using bit shift
 
 // Struct to Store System State
 struct
@@ -16,6 +16,12 @@ struct
 
 // Pin Definitions
 const int RA0_PIN = D3, RA1_PIN = D6, RA2_PIN = D12, REN_PIN = A5, C0_PIN = A2, C1_PIN = D9, C2_PIN = A6, C3_PIN = D1, OUT_PIN = D11, OUTL_PIN = A4, OUTR_PIN = A3, JOYY_PIN = A0, JOYX_PIN = A1;
+
+// Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
 
 // Step Sizes for Notes C5 to B5
 const uint32_t stepSizes[] = {
@@ -41,6 +47,20 @@ U8G2_SSD1305_128X32_ADAFRUIT_F_HW_I2C u8g2(U8G2_R0);
 
 // **Thread Handle**
 TaskHandle_t scanKeysHandle = NULL;
+TaskHandle_t displayTaskHandle = NULL;
+
+// Function to set outputs using key matrix
+void setOutMuxBit(const uint8_t bitIdx, const bool value)
+{
+  digitalWrite(REN_PIN, LOW);
+  digitalWrite(RA0_PIN, bitIdx & 0x01);
+  digitalWrite(RA1_PIN, bitIdx & 0x02);
+  digitalWrite(RA2_PIN, bitIdx & 0x04);
+  digitalWrite(OUT_PIN, value);
+  digitalWrite(REN_PIN, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(REN_PIN, LOW);
+}
 
 // **Interrupt Service Routine for Audio Generation**
 void sampleISR()
@@ -101,6 +121,48 @@ void scanKeysTask(void *pvParameters)
   }
 }
 
+void displayUpdateTask(void *pvParameters)
+{
+  const TickType_t xFrequency = 100 / portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr(2, 10, "Note:");
+    u8g2.setCursor(2, 20);
+
+    int lastPressedKey = -1;
+    for (int i = 0; i < 12; i++)
+    {
+      if (sysState.inputs[i])
+      {
+        lastPressedKey = i;
+      }
+    }
+
+    if (lastPressedKey >= 0)
+    {
+      const char *noteNames[] = {"C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5"};
+      u8g2.print(noteNames[lastPressedKey]);
+    }
+    else
+    {
+      u8g2.print("None");
+    }
+
+    u8g2.setCursor(2, 30);
+    u8g2.print(sysState.inputs.to_ulong(), HEX);
+    u8g2.sendBuffer();
+
+    // Toggle LED (Real-time scheduling indicator)
+    digitalToggle(LED_BUILTIN);
+  }
+}
+
 void setup()
 {
   // **Initialize Pins**
@@ -120,7 +182,11 @@ void setup()
   pinMode(JOYY_PIN, INPUT);
 
   // **Initialize Display**
+  setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
+  delayMicroseconds(2);
+  setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
   u8g2.begin();
+  setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
 
   // **Initialize Serial**
   Serial.begin(9600);
@@ -131,56 +197,13 @@ void setup()
   sampleTimer.attachInterrupt(sampleISR);
   sampleTimer.resume();
 
-  // **Create RTOS Task for Key Scanning**
-  xTaskCreate(
-      scanKeysTask,   // Task function
-      "scanKeys",     // Name
-      64,             // Stack size in words
-      NULL,           // Task parameter
-      1,              // Priority
-      &scanKeysHandle // Handle
-  );
+  xTaskCreate(scanKeysTask, "scanKeys", 128, NULL, 1, &scanKeysHandle);
+  xTaskCreate(displayUpdateTask, "displayUpdate", 256, NULL, 2, &displayTaskHandle);
 
   // **Start RTOS Scheduler**
-  // vTaskStartScheduler();
+  vTaskStartScheduler();
 }
 
 void loop()
 {
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-
-  while (millis() < next)
-    ; // Wait for next interval
-
-  next += interval;
-  // **Update Display (Runs in Main Loop)**
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
-  u8g2.drawStr(2, 10, "Note:");
-  u8g2.setCursor(2, 20);
-
-  int lastPressedKey = -1;
-  for (int i = 0; i < 12; i++)
-  {
-    if (sysState.inputs[i])
-    {
-      lastPressedKey = i;
-    }
-  }
-
-  if (lastPressedKey >= 0)
-  {
-    const char *noteNames[] = {"C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5"};
-    u8g2.print(noteNames[lastPressedKey]);
-  }
-  else
-  {
-    u8g2.print("None");
-  }
-
-  u8g2.setCursor(2, 30);
-  u8g2.print(sysState.inputs.to_ulong(), HEX);
-  u8g2.sendBuffer();
-  digitalToggle(LED_BUILTIN);
 }
